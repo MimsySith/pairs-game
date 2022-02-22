94b330024252dd12c31e02c2e0fddb0e49b40953
Miriam <miriam@smithydrafting.com.au>
2022-02-22 13:19:46 +1100

    Added Fisher-Yates algorithm  to shuffle cards

--- src/app/beginner-level/beginner-level.component.ts
+++ src/app/beginner-level/beginner-level.component.ts
@@ -8,7 +8,9 @@ import { beginnerCards } from '../cards';
 })
 export class BeginnerLevelComponent implements OnInit {
 
-  Cards = beginnerCards
+  Cards = beginnerCards;
+  Count = 0;
+  newArray = this.shuffle([...this.Cards])
 
   constructor() { }
 
@@ -16,7 +18,28 @@ export class BeginnerLevelComponent implements OnInit {
   }
 
   toggle(card: HTMLImageElement, front: HTMLImageElement, url: string){
+    this.Count++;
     card.classList.toggle('show');
-    front.classList.toggle('hide')
+    front.classList.toggle('hide');
+
   }
+
+    //I just couldn't figure out the way to shuffle the cards when I wanted to start a new game.
+    //The following is taken (with slight modifications) from www.webtips.dev/memory-game-in-javascript
+    //it is the Fisher-Yates Shuffle for ECMAScript 2015
+    shuffle(array: any){
+      const newArray = [...array];
+      for(let i=newArray.length-1; i>0; i--){
+          const randomIndex = Math.floor(Math.random()*(i+1));
+          const origin = newArray[i];
+
+          newArray[i] = newArray[randomIndex];
+          newArray[randomIndex] = origin;
+      };
+
+      return newArray;
+
+  };
 }
+
+
