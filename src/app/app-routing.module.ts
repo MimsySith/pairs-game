54a26abecc76771c7a71e440687576b22c13a161
Miriam <miriam@smithydrafting.com.au>
2022-02-22 16:15:00 +1100

    Added Expert level using Intermediate as template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BeginnerLevelComponent } from './beginner-level/beginner-level.component';
 import { EntryPageComponent } from './entry-page/entry-page.component';
+import { ExpertLevelComponent } from './expert-level/expert-level.component';
 import { IntermediateLevelComponent } from './intermediate-level/intermediate-level.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: EntryPageComponent},
   {path: 'beginner', component: BeginnerLevelComponent},
-  {path: 'intermediate', component: IntermediateLevelComponent}
+  {path: 'intermediate', component: IntermediateLevelComponent},
+  {path: 'expert', component: ExpertLevelComponent}
 ];
 
 @NgModule({
