38494debb35868c6d3abf4e867f23baec6287bd8
Miriam <miriam@smithydrafting.com.au>
2022-02-25 08:21:47 +1100

    Added semantic HTML, dynamic alt labels for cards

--- src/app/intermediate-cards.ts
+++ src/app/intermediate-cards.ts
@@ -3,241 +3,288 @@ import { Card } from "./cards";
 export const intermediateCards = [
     {
         name: 'Card-one',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
+        alt: 'dragon head'
     },
 
     {
         name: 'Card-two',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
+        alt: 'minerals'
     },
 
     {
         name: 'Card-three',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
+        alt: 'forward sun'
     },
 
     {
         name: 'Card-four',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
+        alt: 'nautilus shell'
     },
 
     {
         name: 'Card-five',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
+        alt: 'air balloon'
     },
 
     {
         name: 'Card-six',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/ore.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
+        alt: 'ore'
     },
 
     {
         name: 'Card-seven',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
+        alt: 'abbot meeple'
     },
 
     {
         name: 'Card-eight',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
+        alt: 'angry eyes'
     },
 
     {
         name: 'Card-nine',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/ants.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
+        alt: 'ants'
     },
 
     {
         name: 'Card-ten',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
+        alt: 'arabic door'
     },
 
     {
         name: 'Card-eleven',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/banknote.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
+        alt: 'banknote'
     },
 
     {
         name: 'Card-twelve',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/beehive.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
+        alt: 'beehive'
     },
 
     {
         name: 'Card-thirteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-020.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-020.svg',
+        alt: 'abstract twenty'
     },
 
     {
         name: 'Card-fourteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-030.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-030.svg',
+        alt: 'abstract thirty'
     },
 
     {
         name: 'Card-fifteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-100.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-100.svg',
+        alt: 'abstract one hundred'
     },
 
     {
         name: 'Card-sixteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-092.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-092.svg',
+        alt: 'abstract ninety two'
     },
 
     {
         name: 'Card-seventeen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-054.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-054.svg',
+        alt: 'abstract fifty four'
     },
 
     {
         name: 'Card-eighteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-073.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-073.svg',
+        alt: 'abstract seventy three'
     },
 
     {
         name: 'Card-nineteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-004.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-004.svg',
+        alt: 'abstract four'
     },
 
     {
         name: 'Card-twenty',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-111.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-111.svg',
+        alt: 'abstract one hundred and eleven'
     },
 
     {
         name: 'Card-twentyone',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/rihlsul/peanut.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/rihlsul/peanut.svg',
+        abtsract: 'peanut'
     },
 
     {
         name: 'Card-twentytwo',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/big-egg.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/big-egg.svg',
+        alt: 'big egg'
     },
 
     {
         name: 'Card-twentythree',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/clover.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/clover.svg',
+        alt: 'clover'
     },
 
     {
         name: 'Card-twentyfour',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/evil-wings.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/evil-wings.svg',
+        alt: 'evil wings'
     },
-
     {
         name: 'Card-one',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
+        alt: 'dragon head'
     },
 
     {
         name: 'Card-two',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
+        alt: 'minerals'
     },
 
     {
         name: 'Card-three',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
+        alt: 'forward sun'
     },
 
     {
         name: 'Card-four',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
+        alt: 'nautilus shell'
     },
 
     {
         name: 'Card-five',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
+        alt: 'air balloon'
     },
 
     {
         name: 'Card-six',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/ore.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
+        alt: 'ore'
     },
 
     {
         name: 'Card-seven',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
+        alt: 'abbot meeple'
     },
 
     {
         name: 'Card-eight',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
+        alt: 'angry eyes'
     },
 
     {
         name: 'Card-nine',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/ants.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
+        alt: 'ants'
     },
 
     {
         name: 'Card-ten',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
+        alt: 'arabic door'
     },
 
     {
         name: 'Card-eleven',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/banknote.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
+        alt: 'banknote'
     },
 
     {
         name: 'Card-twelve',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/beehive.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
+        alt: 'beehive'
     },
 
     {
         name: 'Card-thirteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-020.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-020.svg',
+        alt: 'abstract twenty'
     },
 
     {
         name: 'Card-fourteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-030.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-030.svg',
+        alt: 'abstract thirty'
     },
 
     {
         name: 'Card-fifteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-100.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-100.svg',
+        alt: 'abstract one hundred'
     },
 
     {
         name: 'Card-sixteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-092.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-092.svg',
+        alt: 'abstract ninety two'
     },
 
     {
         name: 'Card-seventeen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-054.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-054.svg',
+        alt: 'abstract fifty four'
     },
 
     {
         name: 'Card-eighteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-073.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-073.svg',
+        alt: 'abstract seventy three'
     },
 
     {
         name: 'Card-nineteen',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-004.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-004.svg',
+        alt: 'abstract four'
     },
 
     {
         name: 'Card-twenty',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-111.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/viscious-speed/abstract-111.svg',
+        alt: 'abstract one hundred and eleven'
     },
 
     {
         name: 'Card-twentyone',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/rihlsul/peanut.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/rihlsul/peanut.svg',
+        abtsract: 'peanut'
     },
 
     {
         name: 'Card-twentytwo',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/big-egg.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/big-egg.svg',
+        alt: 'big egg'
     },
 
     {
         name: 'Card-twentythree',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/clover.svg'
+        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/clover.svg',
+        alt: 'clover'
     },
 
     {
         name: 'Card-twentyfour',
-        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/evil-wings.svg'
-    },
+        pictureurl: 'assets/icons/ffffff/000000/1x1/sbed/evil-wings.svg',
+        alt: 'evil wings'
+    }
 ]
