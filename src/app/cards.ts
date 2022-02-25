79d11ba1bda6a95c776731f6267d056fab9d37dd
Miriam <miriam@smithydrafting.com.au>
2022-02-26 09:14:24 +1100

    More changes to urls

--- src/app/cards.ts
+++ src/app/cards.ts
@@ -7,217 +7,217 @@ export interface Card{
 export const Cards = [
     {
         name: 'Card-one',
-        pictureurl: './pairs-game/./pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
+        pictureurl: './pairs-game/src/./pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
         alt: 'dragon head'
     },
 
     {
         name: 'Card-two',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
         alt: 'minerals'
     },
 
     {
         name: 'Card-three',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
         alt: 'forward sun'
     },
 
     {
         name: 'Card-four',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
         alt: 'nautilus shell'
     },
 
     {
         name: 'Card-five',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
         alt: 'air balloon'
     },
 
     {
         name: 'Card-six',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
         alt: 'ore'
     },
 
     {
         name: 'Card-seven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
         alt: 'abbot meeple'
     },
 
     {
         name: 'Card-eight',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
         alt: 'angry eyes'
     },
 
     {
         name: 'Card-nine',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
         alt: 'ants'
     },
 
     {
         name: 'Card-ten',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
         alt: 'arabic door'
     },
 
     {
         name: 'Card-eleven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
         alt: 'banknote'
     },
 
     {
         name: 'Card-twelve',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
         alt: 'beehive'
     },
 
     {
         name: 'Card-thirteen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-020.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-020.svg',
         alt: 'abstract twenty'
     },
 
     {
         name: 'Card-fourteen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-030.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-030.svg',
         alt: 'abstract thirty'
     },
 
     {
         name: 'Card-fifteen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-100.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-100.svg',
         alt: 'abstract one hundred'
     },
 
     {
         name: 'Card-sixteen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-092.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-092.svg',
         alt: 'abstract ninety two'
     },
 
     {
         name: 'Card-seventeen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-054.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-054.svg',
         alt: 'abstract fifty four'
     },
 
     {
         name: 'Card-eighteen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-073.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-073.svg',
         alt: 'abstract seventy three'
     },
 
     {
         name: 'Card-nineteen',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-004.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-004.svg',
         alt: 'abstract four'
     },
 
     {
         name: 'Card-twenty',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-111.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/viscious-speed/abstract-111.svg',
         alt: 'abstract one hundred and eleven'
     },
 
     {
         name: 'Card-twentyone',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/rihlsul/peanut.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/rihlsul/peanut.svg',
         abtsract: 'peanut'
     },
 
     {
         name: 'Card-twentytwo',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/big-egg.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/big-egg.svg',
         alt: 'big egg'
     },
 
     {
         name: 'Card-twentythree',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/clover.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/clover.svg',
         alt: 'clover'
     },
 
     {
         name: 'Card-twentyfour',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/evil-wings.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/evil-wings.svg',
         alt: 'evil wings'
     },
 
     {
         name: 'Card-twentyfive',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/flake.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/flake.svg',
         alt: 'flake'
     },
 
     {
         name: 'Card-twentysix',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/fireflake.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/fireflake.svg',
         alt: 'fire flake'
     },
 
     {
         name: 'Card-twentyseven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/falling.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/falling.svg',
         alt: 'human falling'
     },
 
     {
         name: 'Card-twentyeight',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/nodular.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/nodular.svg',
         alt: 'nodular'
     },
 
     {
         name: 'Card-twentynine',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/nuclear.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/nuclear.svg',
         alt: 'nuclear'
     },
 
     {
         name: 'Card-thirty',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/poison.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/poison.svg',
         alt: 'poison'
     },
 
     {
         name: 'Card-thirtyone',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/snorkel.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/snorkel.svg',
         alt: 'snorkel'
     },
 
     {
         name: 'Card-thirtytwo',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/sbed/water-drop.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/sbed/water-drop.svg',
         alt: 'water drop'
     },
 
     {
         name: 'Card-thirtythree',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/guard13007/soda-can.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/guard13007/soda-can.svg',
         alt: 'soda can'
     },
 
     {
         name: 'Card-thirtyfour',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/generalace135/fingers-crossed.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/generalace135/fingers-crossed.svg',
         alt: 'fingers crossed'
     },
 
     {
         name: 'Card-thirtyfive',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/generalace135/shepherds-crook.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/generalace135/shepherds-crook.svg',
         alt: 'shepherds crook'
     },
 
     {
         name: 'Card-thirtysix',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/carl-olsen/flame.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/carl-olsen/flame.svg',
         alt: 'flame'
     },
 ];
@@ -225,145 +225,145 @@ export const Cards = [
 export const beginnerCards = [
     {
         name: 'Card-one',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
         alt: 'dragon head'
     },
 
     {
         name: 'Card-two',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
         alt: 'minerals'
     },
 
     {
         name: 'Card-three',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
         alt: 'forward sun'
     },
 
     {
         name: 'Card-four',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
         alt: 'nautilus shell'
     },
 
     {
         name: 'Card-five',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
         alt: 'air balloon'
     },
 
     {
         name: 'Card-six',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
         alt: 'ore'
     },
 
     {
         name: 'Card-seven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
         alt: 'abbot meeple'
     },
 
     {
         name: 'Card-eight',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
         alt: 'angry eyes'
     },
 
     {
         name: 'Card-nine',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
         alt: 'ants'
     },
 
     {
         name: 'Card-ten',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
         alt: 'arabic door'
     },
 
     {
         name: 'Card-eleven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
         alt: 'banknote'
     },
 
     {
         name: 'Card-twelve',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
         alt: 'beehive'
     },
 
     {
         name: 'Card-one',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/dragon-head.svg',
         alt: 'dragon head'
     },
 
     {
         name: 'Card-two',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/minerals.svg',
         alt: 'minerals'
     },
 
     {
         name: 'Card-three',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/forward-sun.svg',
         alt: 'forward sun'
     },
 
     {
         name: 'Card-four',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/nautilus-shell.svg',
         alt: 'nautilus shell'
     },
 
     {
         name: 'Card-five',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/air-balloon.svg',
         alt: 'air balloon'
     },
 
     {
         name: 'Card-six',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/faithtoken/ore.svg',
         alt: 'ore'
     },
 
     {
         name: 'Card-seven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/abbot-meeple.svg',
         alt: 'abbot meeple'
     },
 
     {
         name: 'Card-eight',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/angry-eyes.svg',
         alt: 'angry eyes'
     },
 
     {
         name: 'Card-nine',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/ants.svg',
         alt: 'ants'
     },
 
     {
         name: 'Card-ten',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/arabic-door.svg',
         alt: 'arabic door'
     },
 
     {
         name: 'Card-eleven',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/banknote.svg',
         alt: 'banknote'
     },
 
     {
         name: 'Card-twelve',
-        pictureurl: './pairs-game/assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
+        pictureurl: './pairs-game/src/assets/icons/ffffff/000000/1x1/delapouite/beehive.svg',
         alt: 'beehive'
     }
 ]
